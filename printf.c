a8df4eef8d59aeb44e356a18e2c12cdc89dd6433
nobulp <lucaslupon873@gmail.com>
2025-11-26 00:14:34 +0000

    chore: ignore local Betty folder

--- printf.c
+++ printf.c
@@ -6,7 +6,13 @@ int _printf(const char *format, ...)
     va_list args;
     int i=0, count = 0;
 }
-if (format[i]== 'c')
+if (format[i] == 'c')
 {
     count += print_char(args);
+    //.....
+}
+else if (format[i] == 's')
+{
+    count += print_string(args);
+    //.....
 }
